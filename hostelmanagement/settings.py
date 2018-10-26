96bbe590c0e1f93fbfaeb1263aa0112459898336
Shubham Singh <shubhamsingh4204@gmail.com>
2018-10-26 20:50:10 +0530

    Add Static url for retrieve static file

--- hostelmanagement/settings.py
+++ hostelmanagement/settings.py
@@ -125,7 +125,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", 'static_root')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", 'media_root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
