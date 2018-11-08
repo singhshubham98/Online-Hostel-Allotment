062dc3af4d21f0bc54b39b5ff4d90455b06d4e85
Shubham Singh <201652026@iiitvadodara.ac.in>
2018-11-08 19:22:55 +0530

    Update admin.py

--- hostel/admin.py
+++ hostel/admin.py
@@ -1,13 +1,1 @@
-from __future__ import unicode_literals
-from .models import Student, Room, Diff, Swap, Change
-from django.contrib import admin
-from django.contrib.auth.models import Group, User
 
-admin.site.register(Student)
-admin.site.register(Room)
-admin.site.register(Diff)
-admin.site.register(Swap)
-admin.site.register(Change)
-# customization of Django Admin
-admin.site.unregister(Group)
-admin.site.site_header='Hostel Management Dashboard'
