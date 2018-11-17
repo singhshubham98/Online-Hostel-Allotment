30baf02e5999f95ffc8e9af1ad10db39f8f07625
Shubham Singh <shubhamsingh4204@gmail.com>
2018-11-17 09:32:06 +0530

    Update settings

--- hostelmanagement/settings.py
+++ hostelmanagement/settings.py
@@ -127,3 +127,11 @@ STATICFILES_DIRS = [
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Confirmation Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'shubhamsingh4204@gmail.com'
+EMAIL_HOST_PASSWORD = 'Personal@Gmail'
+EMAIL_PORT = 587
